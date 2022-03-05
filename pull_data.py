e9a804b4bd0333b7f364b688ff4275ca0796389a
T.J. Gaffney <gaffney.tj@gmail.com>
2022-03-05 12:48:35 -0600

    Pull regular season

--- pull_data.py
+++ pull_data.py
@@ -1,27 +1,79 @@
-from typing import List
+from datetime import datetime
+from typing import Dict, List
 
+import attr
 from bs4 import BeautifulSoup
+import dateutil
+import pandas as pd
 
 from tools import scraper_tools
 
 
+# TODO: Rename these.
 FULL_YEAR = "https://www.sports-reference.com/cbb/seasons/{}-school-stats.html"
-School = int
+SCHEDULE = "https://www.sports-reference.com/cbb/schools/{}/{}-schedule.html"
+
+School = str
+Year = int
+
+
+@attr.s(frozen=True)
+class Game(object):
+    winner: School = attr.ib()
+    loser: School = attr.ib()
+    date: datetime = attr.ib()
 
 
 print("HELLO")
 
-def get_schools(year: int) -> List[School]:
+
+def get_schools(year: Year) -> Dict[str, School]:
+    # All schools in a given year mapped from representative string to the school
     html = scraper_tools.read_url_to_string(FULL_YEAR.format(year))
     soup = BeautifulSoup(html, features="lxml")
     raw_schools = soup.find_all("td", {"data-stat": "school_name"})
-    schools = list()
+    schools = dict()
     for school in raw_schools:
-        link = school.find("a")["href"]
-        school = link.split("/")[3]
-        schools.append(school)
+        link = school.find("a")
+        school_str = link.contents[0]
+        school = link["href"].split("/")[3]
+        schools[school_str] = school
     return schools
 
-print(get_schools(2021))
+
+# TODO: Handle errors better
+def get_reg_season_school(school_dict: Dict[str, School], school: School, year: Year) -> List[Game]:
+    html = scraper_tools.read_url_to_string(SCHEDULE.format(school, year))
+    df = pd.read_html(html)[-1]
+
+    all_games = list()
+    for _, row in df.iterrows():
+        if row["Type"] != "REG":
+            continue
+
+        opponent = school_dict[row["Opponent"].split("\xa0")[0]]
+        outcome = row["Unnamed: 7"]
+        if outcome == "W":
+            winner, loser = school, opponent
+        elif outcome == "L":
+            winner, loser = opponent, school
+        else:
+            # Unanticipated error
+            raise Exception
+
+        date = dateutil.parser.parse(row["Date"])
+
+        game = Game(winner=winner, loser=loser, date=date)
+        all_games.append(game)
+
+    return all_games
+
+
+
+school_dict = get_schools(2021)
+school = "west-virginia"
+year = 2021
+print(get_reg_season_school(school_dict, school, year))
+
 
 print("GOODBYE")
