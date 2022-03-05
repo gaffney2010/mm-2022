f38f16811f7215c93f420a301c62895872dff5af
T.J. Gaffney <gaffney.tj@gmail.com>
2022-03-04 23:41:50 -0600

    Basic files

---
+++ constants.py
@@ -0,0 +1,10 @@
+"""Some constants that will be shared potentially across files."""
+
+import os
+
+# If downloading, you need to set this directory to equal the directory that
+#  houses this file.
+from computer_constants import TOP_LEVEL_DIR
+
+DATA_DIR = os.path.join(TOP_LEVEL_DIR, "data")
+LOGGING_DIR = os.path.join(TOP_LEVEL_DIR, "logging")
---
+++ tools/scraper_tools.py
@@ -0,0 +1,67 @@
+import logging
+import time
+
+import retrying
+from selenium import webdriver
+
+from tools import cache
+from constants import *
+
+DRIVER_DELAY_SEC = 3
+
+
+class WebDriver(object):
+    """Used for convenient open and closing.
+    This will handle closing for you, but will close as soon as the driver
+    leaves scope.
+    """
+
+    def __init__(self):
+        self._driver = None
+
+    def __enter__(self):
+        """We just need this, so that we can have the exit semantic."""
+        return self
+
+    def driver(self):
+        """Get the main object, loading if necessary."""
+        # Lazy load
+        if self._driver is None:
+            logging.debug("Initializing driver.")
+            options = webdriver.FirefoxOptions()
+            options.add_argument('--headless')
+            self._driver = webdriver.Firefox(
+                options=options,
+                service_log_path="{}/geckodriver.log".format(LOGGING_DIR))
+            logging.debug("Finished initializing driver.")
+        return self._driver
+
+    def __exit__(self, type, value, tb):
+        """Clean-up on exit."""
+        if self._driver:
+            self._driver.quit()
+        self._driver = None
+
+
+def read_url_to_string(url: str) -> str:
+    """ Read from a url and print to a string, after fully buffering.
+    If errors after three tries, then will return an empty string.
+    Args:
+        url: The URL to read.
+    Returns:
+         The body of the resulting HTML in a flat string.
+    """
+
+    @cache.memoize(url, cache.BasicCacher())
+    @retrying.retry(wait_random_min=200, wait_random_max=400,
+                    stop_max_attempt_number=3)
+    def read_url_to_string_helper(help_url, web_driver):
+        web_driver.driver().get(help_url)
+        time.sleep(DRIVER_DELAY_SEC)
+        return web_driver.driver().page_source
+
+    logging.debug(f"Reading URL: {url}")
+    with WebDriver() as driver:
+        page_text = read_url_to_string_helper(url, driver)
+    logging.debug("Finished pulling URL.")
+    return page_text
