ebf5aec67c98a87e5ffa637cb809cd1053346f04
T.J. Gaffney <gaffney.tj@gmail.com>
2022-03-12 13:17:33 -0600

    Cleanup conf_bt

--- conf_bt.py
+++ conf_bt.py
@@ -1,6 +1,16 @@
+################################################################################
+# Logging logic, must come first
+from tools.logger import configure_logging
+import logging
+
+configure_logging(
+    screen=False, file=True, screen_level=logging.DEBUG, file_level=logging.WARNING
+)
+################################################################################
+
 # This emulates the code in bradley_terry.py
 import random
-from typing import Any, Iterator
+from typing import Iterator
 
 import attr
 import numpy as np
@@ -17,51 +27,51 @@ class ConfMatch(object):
     loser_conf: Conf = attr.ib()
 
 
-def _na(x: Any) -> bool:
-    return x == "" or x != x
-
-
 def conference_matches(year: Year) -> Iterator[Conf]:
     conf = pull_season.get_conf(year)
     for game in pull_season.scrape_season(year):
         if game.winner not in conf:
-            # TODO: print -> logging statements
-            print(f"No key for {game.winner}")
+            logging.debug(f"No key for {game.winner}")
             continue
         if game.loser not in conf:
-            print(f"No key for {game.loser}")
+            logging.debug(f"No key for {game.loser}")
             continue
         winner_conf, loser_conf = conf[game.winner], conf[game.loser]
         if winner_conf == loser_conf:
-            print("Same conference")
+            logging.debug("Same conference")
             continue
         yield ConfMatch(winner_conf=winner_conf, loser_conf=loser_conf)
 
 
+def conf_bt_season(year: Year) -> Dict[School, float]:
+    # Need this as a list actually:
+    matches = list(conference_matches(year))
+
+    ind_conf = bradley_terry.Indexer()
+    for match in matches:
+        ind_conf.push(match.winner_conf)
+        ind_conf.push(match.winner_conf)
+    num_data_pts = len(matches)
+
+    X, y = np.zeros((num_data_pts, len(ind_conf))), list()
+    for i, match in enumerate(matches):
+        target_winner = random.random() < 0.5
+        if target_winner:
+            X[i, ind_conf.get(match.winner_conf)] = 1
+            X[i, ind_conf.get(match.loser_conf)] = -1
+            y.append(1)
+        else:
+            X[i, ind_conf.get(match.winner_conf)] = -1
+            X[i, ind_conf.get(match.loser_conf)] = 1
+            y.append(0)
+
+    model = LogisticRegression().fit(X, y)
+
+    result = dict()
+    for k, v in ind_conf.ind.items():
+        result[k] = model.coef_[0][v]
+    return result
+
+
 year = 2021
-
-# Need this as a list actually:
-matches = list(conference_matches(year))
-
-ind_conf = bradley_terry.Indexer()
-for match in matches:
-    ind_conf.push(match.winner_conf)
-    ind_conf.push(match.winner_conf)
-num_data_pts = len(matches)
-
-X, y = np.zeros((num_data_pts, len(ind_conf))), list()
-for i, match in enumerate(matches):
-    target_winner = random.random() < 0.5
-    if target_winner:
-        X[i, ind_conf.get(match.winner_conf)] = 1
-        X[i, ind_conf.get(match.loser_conf)] = -1
-        y.append(1)
-    else:
-        X[i, ind_conf.get(match.winner_conf)] = -1
-        X[i, ind_conf.get(match.loser_conf)] = 1
-        y.append(0)
-
-model = LogisticRegression().fit(X, y)
-
-for k, v in ind_conf.ind.items():
-    print(f"{k}: {model.coef_[0][v]}")
+print(conf_bt_season(year))
