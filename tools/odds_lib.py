02bb263c2812de7cba211e1ccccd47ce664dc329
T.J. Gaffney <gaffney.tj@gmail.com>
2022-04-03 16:16:20 -0500

    Cleanup odds_lib

--- tools/odds_lib.py
+++ tools/odds_lib.py
@@ -1,35 +1,24 @@
 import numpy as np
 
 
-def _odds_str(odds: int) -> str:
-    # TODO: Probably should just update these.
-    # For backwards compatibility with historical functions
-    if odds > 0:
-        return f"+{odds}"
-    return str(odds)
-
-
 def lay_from_odds(odds: int) -> float:
-    odds = _odds_str(odds)
-    d = int(odds[1:]) / 100
+    d = abs(odds) / 100
     d = np.sqrt(d)
-    if odds[0] == "+":
+    if odds > 0:
         return 1.0 / d
     return d
 
 
 def prob_from_odds(odds: int) -> float:
     """Assumes even-money"""
-    odds = _odds_str(odds)
-    d = int(odds[1:]) / 100
-    if odds[0] == "+":
+    d = abs(odds) / 1000
+    if odds > 0:
         return 1 / (1 + d)
     return d / (1 + d)
 
 
 def win_multiplier(odds: int) -> float:
-    odds = _odds_str(odds)
-    d = int(odds[1:]) / 100
-    if odds[0] == "+":
+    d = abs(odds) / 100
+    if odds > 0:
         return d
     return 1 / d
