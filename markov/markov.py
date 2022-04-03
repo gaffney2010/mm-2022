a1575a09ceba91490461fd572451e302616ebef0
T.J. Gaffney <gaffney.tj@gmail.com>
2022-04-03 01:32:54 -0500

    Finish markov.py

--- markov/markov.py
+++ markov/markov.py
@@ -1,8 +1,9 @@
 from collections import defaultdict
 import random
-from typing import Any, Callable, DefaultDict, Dict, List
+from typing import Any, Callable, DefaultDict, Dict, List, Tuple
 
 import attr
+import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
@@ -54,37 +55,61 @@ Action = Callable[[State], 'Node']
 
 # TODO: Run black
 class Node(object):
-    # TODO: Consider making these sets.
     def __init__(self, id: NodeId, states: List[StateVar], actions: List[ActionId]):
         self.id = id
         self.states = states
+        self.state_values: Dict[StateVar, List] = dict()
+        self.num_values: int = 0
         self.actions = actions
         self.histogram = Histogram()
         self.model = None
 
+    def _row_from_state(self, state: State) -> np.ndarray:
+        assert self.num_values > 0
+        result = np.zeros(self.num_values)
+        i = 0
+        for s in self.states:
+            for v in self.state_values[s]:
+                if state[s] == v:
+                    result[i] = 1
+                i += 1
+        return result
+
     # TODO: Profile and improve
     def train(self, data: List['Datum']) -> None:
-        rows = list()
+        # Start with assertions
         for datum in data:
             assert datum.node_id == self.id
             assert datum.action_id in self.actions
-            row = dict()
-            for s in self.states:
-                assert s in datum.state
-                row[s] = datum.state[s]
-            row["target"] = datum.action_id
-            rows.append(row)
-
-            self.histogram.push(datum.duration)
-        df = pd.DataFrame(rows)
-
-        dummy_dfs = list()
         for s in self.states:
-            dummy_dfs.append(pd.get_dummies(df[s]))
-        X = pd.concat(dummy_dfs, axis=1)
-        y = df["target"]
+            assert s in datum.state
+
+        # Prepare state_values
+        state_values = defaultdict(set)
+        for datum in data:
+            for s in self.states:
+                state_values[s].add(datum.state[s])
+        self.state_values = {k: list(v) for k, v in state_values.items()}
+        for v in state_values.values:
+            self.num_values += len(v)
+
+        X = np.stack((self._row_from_datum(datum) for datum in data))
+        y = [datum.action_id for datum in data]
 
         self.model = LogisticRegression().fit(X, y)
+
+    def simulate(self, state: State) -> Tuple[Second, ActionId]:
+        assert self.model is not None
+        X = [self._row_from_state(state)]
+        pred = self.model.predict_proba(X)
+        r = random.random()
+        for action, prob in zip(self.model.classes, pred):
+            r -= prob
+            # TODO: Replace this with a small epsilon to handle rounding error corner cases.
+            if r <= 0:
+                return self.histogram.sample(), action
+        else:
+            raise MarkovError("No action selected.")
 
 
 @attr.s(frozen=True)
@@ -110,8 +135,26 @@ class Graph(object):
         self.actions[id] = action
 
     def train(self, data: List[Datum]) -> None:
-        pass
+        for node in self.nodes.values():
+            node.train(data)
 
     # TODO: Import School type
     def simulate(self, teams: List[str]) -> Dict[str, float]:
-        pass
+        # Special states
+        self.state["_teams"] = teams
+        self.state["_scores"] = {t: 0 for t in teams}
+
+        # Two halfs
+        for _ in range(2):
+            # TODO: Use constants instead of literals
+            clock = 60 * 20
+            node_id = self.actions["tip-off"](self.state)
+
+            while True:
+                duration, action_id = self.nodes[node_id].simulate(self.state)
+                clock -= duration
+                if clock < 0:
+                    break
+                node_id = self.actions[action_id](self.state)
+
+        return self.state["_scores"]
